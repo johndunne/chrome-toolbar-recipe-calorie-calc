8a4a98baee199cdf6cb9f7ddb63f0fb7de16c711
johndunne <john.dunne@epexcel.com>
2015-12-13 17:12:59 +0000

    More files for ignoring

--- createChromeApp.sh
+++ createChromeApp.sh
@@ -3,3 +3,6 @@
 rm -rf dist
 mkdir dist
 rsync --exclude dist --exclude .DS_Store --exclude bower_components --exclude .idea --exclude .git --exclude .gitignore --exclude pullSupportedSites.sh --exclude sites.list --exclude *.sh -av . dist
+
+mv dist RecipeCalCalc
+zip -r recipecalcalc.zip RecipeCalCalc
